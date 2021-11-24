a74f19ebe65af74d7c1ad7c188ee3fa9bfdcd143
Mohammad Iliyaz <mohammadiliyaz1210@gmail.com>
2021-11-24 22:30:40 +0530

    this is armstrong

---
+++ ArmStrong.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class ArmStrong // 153,370 are examples for armstrong.
+{
+	public static void main(String[] args) 
+	{
+		int rem,sum=0;
+		Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Number ");
+		int num = sc.nextInt();
+		int originalNum=num;
+		while(num!=0)
+		{
+			rem=num%10;
+			sum=sum+(rem*rem*rem);
+			num=num/10;
+		}
+		if(sum==originalNum)
+		{
+		System.out.println(originalNum+" is an Armstrong Number");
+		}
+		else
+			System.out.println(originalNum+" Not a Armstrong Number");
+	}
+}
