8989e5b99ad6be1ede5af4c0265ed71d45f0d583
iliyazahmed <91978876+iliyazahmed@users.noreply.github.com>
2021-11-30 09:35:37 +0530

    Factorial Update

---
+++ Factorial.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class Factorial 
+{
+	public static void main(String[] args) 
+	{
+		int fact=1;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Number to check factorial series....");
+        int num = sc.nextInt();
+		for (int i=1;i<=num ;i++ )
+		{
+          fact = fact*i;    
+		}
+		System.out.println("Factorial Series of "+num+" is "+fact);
+
+
+	}
+}
